ecc1ba01a1eaa84b1edbe88bbb2200e3f7fa5f2c
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-04-16 08:34:22 +0200

    maj timer

--- app/components/board/board.component.js
+++ app/components/board/board.component.js
@@ -1,19 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native';
 
+import PlayerTimer from './timers/player-timer.component';
+import OpponentTimer from './timers/opponent-timer.component';
+
 
 const OpponentInfos = () => {
   return (
     <View style={ styles.opponentInfosContainer }>
       <Text>Opponent infos</Text>
-    </View>
-  );
-};
-
-const OpponentTimer = () => {
-  return (
-    <View style={ styles.opponentTimerContainer }>
-      <Text>Timer: </Text>
     </View>
   );
 };
@@ -65,15 +60,6 @@ const PlayerInfos = () => {
   return (
     <View style={ styles.playerInfosContainer }>
       <Text>Player Infos</Text>
-    </View>
-  );
-};
-
-const PlayerTimer = () => {
-
-  return (
-    <View style={ styles.playerTimerContainer }>
-      <Text>Timer: </Text>
     </View>
   );
 };
