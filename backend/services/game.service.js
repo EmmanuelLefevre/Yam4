b4d73d48324b14cf01f7b43d8ae42ea27a3b7efe
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-04-16 14:08:46 +0200

    maj

--- backend/services/game.service.js
+++ backend/services/game.service.js
@@ -25,6 +25,30 @@ const GAME_INIT = {
   }
 }
 
+const CHOICES_INIT = {
+  isDefi: false,
+  isSec: false,
+  idSelectedChoice: null,
+  availableChoices: [],
+};
+
+const ALL_COMBINATIONS = [
+  { value: 'Brelan1', id: 'brelan1' },
+  { value: 'Brelan2', id: 'brelan2' },
+  { value: 'Brelan3', id: 'brelan3' },
+  { value: 'Brelan4', id: 'brelan4' },
+  { value: 'Brelan5', id: 'brelan5' },
+  { value: 'Brelan6', id: 'brelan6' },
+  { value: 'Full', id: 'full' },
+  { value: 'Carré', id: 'carre' },
+  { value: 'Yam', id: 'yam' },
+  { value: 'Suite', id: 'suite' },
+  { value: '≤8', id: 'moinshuit' },
+  { value: 'Sec', id: 'sec' },
+  { value: 'Défi', id: 'defi' }
+];
+
+
 const GameService = {
 
   init: {
@@ -38,6 +62,10 @@ const GameService = {
     deck: () => {
       return { ...DECK_INIT };
     },
+
+    choices: () => {
+      return { ...CHOICES_INIT };
+    }
   },
   send: {
     forPlayer: {
@@ -48,18 +76,6 @@ const GameService = {
           idPlayer: (playerKey === 'player:1') ? game.player1Socket.id : game.player2Socket.id,
           idOpponent: (playerKey === 'player:1') ? game.player2Socket.id : game.player1Socket.id
         };
-      },
-
-      deckViewState: (playerKey, gameState) => {
-        const deckViewState = {
-            displayPlayerDeck: gameState.currentTurn === playerKey,
-            displayOpponentDeck: gameState.currentTurn !== playerKey,
-            displayRollButton: gameState.deck.rollsCounter <= gameState.deck.rollsMaximum,
-            rollsCounter: gameState.deck.rollsCounter,
-            rollsMaximum: gameState.deck.rollsMaximum,
-            dices: gameState.deck.dices
-        };
-        return deckViewState;
       },
 
       viewQueueState: () => {
@@ -94,6 +110,15 @@ const GameService = {
         };
         return deckViewState;
       },
+
+      choicesViewState: (playerKey, gameState) => {
+        return {
+            displayChoices: true,
+            canMakeChoice: playerKey === gameState.currentTurn,
+            idSelectedChoice: gameState.choices.idSelectedChoice,
+            availableChoices: gameState.choices.availableChoices
+        };
+      }
     }
   },
   timer: {
@@ -139,6 +164,48 @@ const GameService = {
         locked: true
       }));
       return lockedDices;
+    }
+  },
+  choices: {
+    findCombinations: (dices, isDefi, isSec) => {
+
+        const allCombinations = ALL_COMBINATIONS;
+
+        // Tableau des objets 'combinations' disponibles parmi 'ALL_COMBINATIONS'
+        const availableCombinations = [];
+
+        // Tableau pour compter le nombre de dés de chaque valeur (de 1 à 6)
+        const counts = Array(7).fill(0);
+
+        let hasPair = false; // check: paire
+        let threeOfAKindValue = null; // check: valeur brelan
+        let hasThreeOfAKind = false; // check: brelan
+        let hasFourOfAKind = false; // check: carré
+        let hasFiveOfAKind = false; // check: yam
+        let hasStraight = false; // check: suite
+        let isLessThanEqual8 = false;
+        let sum = 0; // sum of dices
+
+        // -----------------------------------
+        // TODO: Vérifier les combinaisons possibles
+        // -----------------------------------
+
+        // return available combinations
+        allCombinations.forEach(combination => {
+            if (
+                (combination.id.includes('brelan') && hasThreeOfAKind && parseInt(combination.id.slice(-1)) === threeOfAKindValue) ||
+                (combination.id === 'full' && hasPair && hasThreeOfAKind) ||
+                (combination.id === 'carre' && hasFourOfAKind) ||
+                (combination.id === 'yam' && hasFiveOfAKind) ||
+                (combination.id === 'suite' && hasStraight) ||
+                (combination.id === 'moinshuit' && isLessThanEqual8) ||
+                (combination.id === 'defi' && isDefi)
+            ) {
+                availableCombinations.push(combination);
+            }
+        });
+
+        return availableCombinations;
     }
   },
   utils: {
