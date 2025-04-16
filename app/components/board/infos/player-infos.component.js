6a825250fe3fa1b93cd3b8ba2cd65ffab5d70ae1
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-04-16 11:06:51 +0200

    typo

--- app/components/board/infos/player-infos.component.js
+++ app/components/board/infos/player-infos.component.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 const PlayerInfos = () => {
   return (
     <View style={ styles.playerInfosContainer }>
-      <Text>Player Infos</Text>
+      <Text>Player</Text>
     </View>
   );
 };
