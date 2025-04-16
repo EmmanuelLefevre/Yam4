ee2b94d1b442d70ae5e1732ac48d30d45bc1ffbc
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-04-16 08:34:40 +0200

    maj timer

--- app/components/board/timers/player-timer.component.js
+++ app/components/board/timers/player-timer.component.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import { SocketContext } from '../contexts/socket.context';
+import { SocketContext } from '../../../contexts/socket.context';
 
 
 const PlayerTimer = () => {
