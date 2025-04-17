a0b3134437ed07ee52e702ab2aac4b4c7ef7f913
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-04-17 09:15:20 +0200

    maj

--- app/screens/home.screen.js
+++ app/screens/home.screen.js
@@ -1,21 +1,28 @@
-import { Button, StyleSheet, View } from "react-native";
+import { Button, Image, StyleSheet, View } from "react-native";
+
+import logo from "../assets/img/logo_yam4.png";
 
 
 export default function HomeScreen({ navigation }) {
 
   return (
-    <View style={ styles.container }>
-      <View style={ styles.buttonWrapper }>
-        <Button
-          title="Jouer en ligne"
-          onPress={ () => navigation.navigate('OnlineGameScreen') }/>
+    <>
+      <View style={ styles.logoContainer }>
+        <Image source={ logo } style={ styles.logo } resizeMode="contain" />
       </View>
-      <View style={ styles.buttonWrapper }>
-        <Button
-          title="Jouer contre le bot"
-          onPress={ () => navigation.navigate('VsBotGameScreen') }/>
+      <View style={ styles.container }>
+        <View style={ styles.buttonWrapper }>
+          <Button
+            title="Jouer en ligne"
+            onPress={ () => navigation.navigate('OnlineGameScreen') }/>
+        </View>
+        <View style={ styles.buttonWrapper }>
+          <Button
+            title="Jouer contre le bot"
+            onPress={ () => navigation.navigate('VsBotGameScreen') }/>
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
@@ -28,5 +35,13 @@ const styles = StyleSheet.create({
   },
   buttonWrapper: {
     marginVertical: 10,
+  },
+  logoContainer: {
+    marginBottom: 10,
+    alignItems: "center",
+  },
+  logo: {
+    width: 100,
+    height: 40,
   }
 });
