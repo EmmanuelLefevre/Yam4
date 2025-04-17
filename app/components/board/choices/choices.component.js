75c51e3f24f111f55d85e1232554606cb74e8a65
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-04-17 08:39:41 +0200

    maj

--- app/components/board/choices/choices.component.js
+++ app/components/board/choices/choices.component.js
@@ -1,3 +1,4 @@
+import React, { useContext, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import { SocketContext } from "../../../contexts/socket.context";
