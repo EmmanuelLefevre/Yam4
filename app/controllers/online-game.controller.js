c113d49ce1279301eed6eb5e2c8dc4dd2aec91ba
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-04-18 10:47:33 +0200

    maj

--- app/controllers/online-game.controller.js
+++ app/controllers/online-game.controller.js
@@ -85,11 +85,11 @@ export default function OnlineGameController() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
     width: '100%',
     height: '100%',
+    backgroundColor: "#fff",
   },
   paragraph: {
     fontSize: 16,
