2b35c14a9c1f5c072ed667573296e317cd007d23
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-04-18 09:45:36 +0200

    maj

--- app/screens/home.screen.js
+++ app/screens/home.screen.js
@@ -58,7 +58,7 @@ const styles = StyleSheet.create({
   },
   customButton: {
     alignItems: 'center',
-    width: 100,
+    width: 150,
     marginVertical: 10,
     paddingVertical: 10,
     backgroundColor: '#FFF',
