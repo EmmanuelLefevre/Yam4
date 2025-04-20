7b7843da25cbfbfaf0eba7ae51df0ade887f1e56
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-04-20 15:12:57 +0200

    maj

--- app/components/board/decks/dice.component.js
+++ app/components/board/decks/dice.component.js
@@ -10,12 +10,26 @@ const Dice = ({ index, locked, value, onPress, opponent }) => {
     }
   };
 
+  const isInitial = value === null || value === undefined || isNaN(value);
+
   return (
     <TouchableOpacity
-      style={ [styles.dice, locked && styles.lockedDice] }
+      style={[
+        styles.dice,
+        isInitial && styles.initialDice,
+        locked && styles.lockedDice
+      ]}
       onPress={ handlePress }
       disabled={ opponent }>
-      <Text style={ styles.diceText }>{ value }</Text>
+
+      <Text
+        style={[
+          styles.diceText,
+          isInitial && styles.initialDiceText,
+          locked && styles.lockedDiceText
+      ]}>
+        { isInitial ? "?" : value }
+      </Text>
     </TouchableOpacity>
   );
 };
@@ -24,22 +38,41 @@ const styles = StyleSheet.create({
   dice: {
     justifyContent: "center",
     alignItems: "center",
+    color: "#6B6F73",
     width: 40,
     height: 40,
-    backgroundColor: "lightblue",
+    backgroundColor: "#13171A",
+    borderWidth: 1,
+    borderColor: "#ED6A11",
     borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 5,
+      height: 5
+    },
+    shadowOpacity: 0.4,
+    shadowRadius: 6,
+    elevation: 6
+  },
+  initialDice: {
+    backgroundColor: "red",
+    borderWidth: 1,
+    borderColor: "#555"
   },
   lockedDice: {
-    backgroundColor: "gray",
+    backgroundColor: "#ED6A11"
   },
   diceText: {
     fontSize: 20,
-    fontWeight: "bold",
+    color: "#6B6F73",
+    fontWeight: "bold"
   },
-  opponentText: {
-    fontSize: 12,
-    color: "red",
+  initialDiceText: {
+    color: "red"
   },
+  lockedDiceText: {
+    color: "#FFF"
+  }
 });
 
 export default Dice;
