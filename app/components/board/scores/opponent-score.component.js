fced22b35c33fd75cff36a6fbf23aeb9276cf142
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-04-23 17:38:04 +0200

    maj

--- app/components/board/scores/opponent-score.component.js
+++ app/components/board/scores/opponent-score.component.js
@@ -4,14 +4,23 @@ import { useFonts } from 'expo-font';
 import { Chewy_400Regular } from '@expo-google-fonts/chewy';
 
 
-const OpponentScore = () => {
+const OpponentScore = ({ score = 0 }) => {
   useFonts({
     Chewy_400Regular
   });
 
+  const scoreValueStyle = score === 0
+    ? styles.scoreRed
+    : styles.scoreGreen;
+
   return (
     <View style={ styles.opponentScoreContainer }>
-      <Text style={ styles.opponentScore }>Score : </Text>
+      <Text style={ styles.opponentScore }>
+        Score :
+        <Text style={ [styles.scoreValue, scoreValueStyle] }>
+          { ` ${ score }` }
+        </Text>
+      </Text>
     </View>
   );
 };
@@ -28,6 +37,19 @@ const styles = StyleSheet.create({
     fontFamily: "Chewy_400Regular",
     fontWeight: "bold",
     letterSpacing: 1.2
+  },
+  scoreValue: {
+    width: 30,
+    textAlign: "center",
+    display: "inline-block"
+  },
+  scoreGreen: {
+    color: "#8AB70E",
+    fontWeight: "bold"
+  },
+  scoreRed: {
+    color: "#FF0000",
+    fontWeight: "bold"
   }
 });
 
