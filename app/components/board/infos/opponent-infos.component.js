856fb7ad110892c568d3f19cc7000fc90703a4ea
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-04-24 10:48:08 +0200

    maj

--- app/components/board/infos/opponent-infos.component.js
+++ app/components/board/infos/opponent-infos.component.js
@@ -26,7 +26,7 @@ const styles = StyleSheet.create({
     borderColor: "#E66E15"
   },
   opponentText: {
-    fontSize: 19,
+    fontSize: 17,
     color: "#E66E15",
     fontFamily: "Chewy_400Regular",
     fontWeight: "bold",
