11f4b367ee3e261e7c5721568c7532acf40b6966
LefevreEmmanuel <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-04-24 10:48:45 +0200

    maj

--- app/components/board/timers/player-timer.component.js
+++ app/components/board/timers/player-timer.component.js
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   playerTimer: {
-    fontSize: 14,
+    fontSize: 12,
     color: "#E66E15",
     fontFamily: "Chewy_400Regular",
     fontWeight: "bold",
