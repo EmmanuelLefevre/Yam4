a389253e7b92465d1fa5d912a8af3f1259bc28f1
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-04-26 21:52:38 +0200

    maj

--- app/screens/vs-bot-game.screen.js
+++ app/screens/vs-bot-game.screen.js
@@ -107,7 +107,6 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 17,
     color: "#6B6F73",
-    fontFamily: "Chewy_400Regular",
-    fontWeight: "bold"
+    fontFamily: "Chewy_400Regular"
   }
 });
